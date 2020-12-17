2c8dad021ef102a7fe506827a3ccfd1e6b81c046
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-12-17 21:35:01 +0100

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import {ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {StoreService} from './store.service';
-import {environment} from '../environments/environment';
-import 'oidc-client';
-import 'las2peer-frontend-statusbar/las2peer-frontend-statusbar.js';
-import {CordovaPopupNavigator, UserManager} from 'oidc-client';
-import {MediaMatcher} from '@angular/cdk/layout';
-import {MatSidenav, MatSnackBar} from '@angular/material';
-import {SwUpdate} from '@angular/service-worker';
-import {Router} from '@angular/router';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { StoreService } from "./store.service";
+import { environment } from "../environments/environment";
+import "oidc-client";
+import "las2peer-frontend-statusbar/las2peer-frontend-statusbar.js";
+import { CordovaPopupNavigator, UserManager } from "oidc-client";
+import { MediaMatcher } from "@angular/cdk/layout";
+import { MatSidenav, MatSnackBar } from "@angular/material";
+import { SwUpdate } from "@angular/service-worker";
+import { Router } from "@angular/router";
 
 // workaround for openidconned-signin
 // remove when the lib imports with "import {UserManager} from 'oidc-client';" instead of "import 'oidc-client';"
@@ -22,24 +28,30 @@ window.UserManager = UserManager;
 window.CordovaPopupNavigator = CordovaPopupNavigator;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  @ViewChild(MatSidenav, {static: false})
+  @ViewChild(MatSidenav, { static: false })
   public sidenav: MatSidenav;
 
-  title = 'Mensa Guide';
+  title = "Mensa Guide";
   mobileQuery: MediaQueryList;
   mobileQueryListener: () => void;
   user;
   signedIn = false;
   environment = environment;
 
-  constructor(private store: StoreService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private swUpdate: SwUpdate,
-              private snackBar: MatSnackBar, private router: Router) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  constructor(
+    private store: StoreService,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private swUpdate: SwUpdate,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {
+    this.mobileQuery = media.matchMedia("(max-width: 600px)");
     this.mobileQueryListener = () => changeDetectorRef.detectChanges();
     /* tslint:disable-next-line */
     this.mobileQuery.addListener(this.mobileQueryListener);
@@ -66,13 +78,17 @@ export class AppComponent implements OnInit, OnDestroy {
     });*/
     if (this.swUpdate.isEnabled) {
       this.swUpdate.available.subscribe(() => {
-        const snackBarRef = this.snackBar.open('New version available. Reload to update.', 'Reload', null);
+        const snackBarRef = this.snackBar.open(
+          "New version available. Reload to update.",
+          "Reload",
+          null
+        );
         snackBarRef.onAction().subscribe(() => {
           window.location.reload();
         });
       });
     }
-    this.store.user.subscribe(user => {
+    this.store.user.subscribe((user) => {
       this.user = user;
       this.signedIn = !!user;
     });
@@ -87,9 +103,9 @@ export class AppComponent implements OnInit, OnDestroy {
   setUser(user) {
     if (user === null) {
       new UserManager({}).signoutRedirectCallback().then(() => {
-        this.router.navigate(['/']);
+        this.router.navigate(["/"]);
       });
     }
-    this.store.setUser(user);
+    this.store.User(user);
   }
 }

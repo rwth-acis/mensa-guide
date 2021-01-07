08b263b8037f434baf9e686dceb708850414fc70
lakhoune <38857665+lakhoune@users.noreply.github.com>
2021-01-07 19:46:24 +0100

    fix Cannot read property 'apiUrl' of undefined

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,10 @@ export const environment = {
   production: false,
   openIdAuthorityUrl: "https://api.learning-layers.eu/o/oauth2",
   openIdClientId: "f8622260-875b-499a-82db-db55f89f9deb",
-  las2peerWebConnectorUrl: window["env"]["apiUrl"] || "http://127.0.0.1:8080",
+  las2peerWebConnectorUrl:
+    window.hasOwnProperty("env") && window["env"].hasOwnProperty("apiUrl") //check if apiurl is set by docker.
+      ? window["env"]["apiUrl"]
+      : "http://127.0.0.1:8080",
   debug: true,
 };
 

18d3e9d2f803030b85e3d74ff30174629768ed40
lakhoune <38857665+lakhoune@users.noreply.github.com>
2021-02-08 15:19:04 +0100

    hardcode apiurl

--- src/assets/env.js
+++ src/assets/env.js
@@ -3,6 +3,5 @@
 (function (window) {
   window['env'] = window['env'] || {};
   // Environment variables
-  window.window['env']['apiUrl'] =
-    'https://las2peer.tech4comp.dbis.rwth-aachen.de';
+  window.window['env']['apiUrl'] = 'las2peer-bootstrap.sbf-dev:32011';
 })(this);

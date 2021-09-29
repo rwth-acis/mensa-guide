408a95dea6228fc5d240e5806a575dcbb4486aa5
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-09-29 13:57:08 +0200

    remove duplice dishes from dish list

--- src/app/dishes/dishes.component.ts
+++ src/app/dishes/dishes.component.ts
@@ -5,7 +5,9 @@ import { Dish, MensaMenus, MenuItem } from '../models/menu';
 import { Picture } from '../models/picture';
 import { Rating } from '../models/rating';
 import { StoreService } from '../store.service';
-
+interface DishCollection {
+  [name: string]: Dish;
+}
 @Component({
   selector: 'app-dishes',
   templateUrl: './dishes.component.html',
@@ -25,9 +27,20 @@ export class DishesComponent implements OnInit, OnDestroy {
     this.store.initDishes();
     // this.store.startPolling(false);
     this.dishes$ = this.store.dishes.pipe(
-      map((dishes) =>
-        dishes ? dishes.sort((a, b) => a.name.localeCompare(b.name)) : null
-      )
+      map((dishes: Dish[]) =>
+        dishes.reduce(
+          (processedDishes, currDish) =>
+            currDish.name in processedDishes
+              ? processedDishes
+              : (processedDishes[currDish.name] = currDish),
+          {}
+        )
+      ), // create a dictionary of dishes with dish name as key
+      map((dishes: DishCollection) =>
+        dishes
+          ? Object.values(dishes).sort((a, b) => a.name.localeCompare(b.name))
+          : null
+      ) // transform into an array of dishes and sort
     );
     this.store.menu.subscribe((menu) => (this.menu = menu));
     this.store.menuRatings.subscribe((reviews) => (this.reviews = reviews));
